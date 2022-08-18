c68d9c13c49e1850a05055a82e77aa5abd477eb3
Lgoop <lgoop@rocketmail.com>
2022-08-19 07:59:45 +1000

    Updated global script to make current page link highlight work

--- static/js/script-global.js
+++ static/js/script-global.js
@@ -1,7 +1,9 @@
 // find the anchor with the href that matches the current url and add a styling class to it 
 currentLinks = document.querySelectorAll('a');
 currentLinks.forEach(function(link) {
-  if ('/' + link.getAttribute("href") == window.location.pathname && link.className == 'link') {
+  console.log(window.location.pathname.split("/").pop());
+  if (link.getAttribute("href") == window.location.pathname.split("/").pop() && link.className == 'link') {
     link.className += ' currentPage';
+    console.log("test")
   }
 }); 
