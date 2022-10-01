6c255ae1d7701306d45bfff06ef30f86121675b5
Giang <laingngangiang030@gmail.com>
2022-10-01 19:13:01 +1000

    replace login.html with login.php

--- login.php
+++ login.php
@@ -1,8 +1,44 @@
+<?php
+require "dbconn.php";
+//$_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["submit"]) && 
+if (isset($_POST["username"]) && isset($_POST["password"])) {
+    $username = htmlspecialchars($_POST["username"]);
+    $password = htmlspecialchars($_POST["password"]);
+
+    if(authenticate($username, $password) == true) {
+      header("location: 'index.php'");
+    }
+    else {
+        echo "<p>Invalid username or password.</br>Please try again.</p>";
+    }
+}
+
+// Queries the DBMS with the supplied user details
+// Returns true on successful authentication and false otherwise.
+function authenticate($user, $pass)
+{
+    global $conn;
+
+    // Complete this function
+    $sql = "SELECT accountPassword FROM ACCOUNT WHERE accountName='$user'";
+    $result = $conn->query($sql);
+    
+    if ($result->fetch_assoc() == $pass) {
+      return true;
+    }
+    else {
+      return false;
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
 <title>Login</title>
 <head>
   <meta charset="UTF-8">
   <meta name="description" content="">
-  <meta name="author" content="Alex Drew, Giang, Katherine Peschar">
+  <meta name="author" content="Alex Drew, Nguyen Ngan Giang Lai, Katherine Peschar">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
   <!-- Google fonts  -->
@@ -18,12 +54,13 @@
   <!-- FontAwesome Kit for Icons -->
   <script src="https://kit.fontawesome.com/5f78ca6619.js" crossorigin="anonymous"></script>
 </head>
+
 <body>
   <header class="header">
     <a href="index.php">
       <img src="./static/images/logo.svg" alt="logo" class="logo">
     </a>
-    <a href="login.html" class="link">Login</a>
+    <a href="login.php" class="link">Login</a>
     <a href="create.html" class="link">Create a Post!</a>
     <a href="about.html" class="link">About Us</a>
     <a href="archive.php" class="link">Archive</a>
@@ -43,10 +80,10 @@
           <i class="fab fa-linkedin"></i>
         </div>
         <div class="divider"><span>Or login here</span></div>
-        <form class="login-form" name="login-form" action="" novalidate>
+        <form class="login-form" name="login-form" method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
           <input type="text" id="username" name="username" placeholder="Email or Account Name">
           <input type="password" id="pass" name="password" minlength="8" required placeholder="Password">
-          <input class="button" type="submit" value="Sign In"></input>
+          <input class="button" name="submit" type="submit" value="Sign In"></input>
         </form>
         <h2 class="errorMessage" id="errorMessage"></h2>
         <div class="divider"><span>Don't have an account yet?</span></div>
@@ -64,3 +101,4 @@
     <p class="acknowledgment">We acknowledge and pay respect to the traditional custodians and elders of this nation, past, present and future, and the continuation of cultural, spiritual and educational practices of Aboriginal and Torres Strait Islander peoples.</p>
   </footer>
 </body>
+</html>
